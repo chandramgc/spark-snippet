5a757770a049217b283a2d44e03432d461503141
Girish <chandramgc@gmail.com>
2018-10-28 22:57:32 -0400

    git.sh

--- git.sh
+++ git.sh
@@ -1,20 +1,5 @@
 #!/bin/sh
-
 comment=$1
-
-git add .
-
+git add --all
 git commit -m $comment
-
-echo "commit finished,push to origin master  ? "
-
-read commit
-
-case $commit in 
-y|Y|YES|yes)
 git push -u origin master
-;;
-n|NO|N|no)
- exit 0
-
-esac
