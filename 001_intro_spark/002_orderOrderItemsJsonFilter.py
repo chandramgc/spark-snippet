74fc3a55c2113403822c63659d87dd9dce76f473
Girish <chandramgc@gmail.com>
2018-10-30 19:52:37 -0400

    Current commit is on EST  Tue Oct 30 18:52:36 2018 EST

---
+++ 001_intro_spark/002_orderOrderItemsJsonFilter.py
@@ -0,0 +1,32 @@
+import os, sys
+import time
+from pyspark import SparkConf, SparkContext, SQLContext
+startTimeStr = time.strftime("%c")
+fileName = os.path.basename(sys.argv[0]).split('.')[0]
+conf = SparkConf().setAppName(fileName).setMaster("yarn-client").set("spark.ui.port","12888")
+sc = SparkContext(conf=conf)
+sqlContext = SQLContext(sc)
+log4j = sc._jvm.org.apache.log4j
+log4j.LogManager.getRootLogger().setLevel(log4j.Level.WARN)
+#sc.setLogLevel("WARN")
+
+# Code Starting
+
+order_json = sqlContext.read.json("/public/retail_db_json/orders/")
+order_json_copy = sqlContext.read.format("json").load("/public/retail_db_json/orders/")
+
+consolePrint = order
+
+print ""
+print "************************************************************************************************"
+print "Job started at " + startTimeStr
+print ""
+
+for i in consolePrint.take(10) : print(i) 
+
+print ""
+print "Job stoped at " + time.strftime("%c")
+print "************************************************************************************************"
+print ""
+
+# Code Ending
